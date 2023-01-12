5b5c51fd94ff6813b9062a589fc8c810430b1341
KIRILL <kozlov.khv@gmail.com>
2023-01-12 15:19:55 +1000

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import sys
+from random import sample
+from modules import *
+
+
+def main():
+    question_bank = []
+    question_random = sample(question_db, 10)
+    for q in question_random:
+        question_text = q['question']
+        question_answer = q['answer']
+        new_question = Question(question_text, question_answer)
+        question_bank.append(new_question)
+
+    known_tester = KnownTester(question_bank)
+
+    while known_tester.still_has_question():
+        known_tester.next_question()
+
+    print(f'Правильных ответов: {known_tester.score} из {known_tester.question_number}')
+    repeat_test = input('Чтобы повторить тест введите "y", или нажмите на любую клавишу: ')
+    if repeat_test == 'y':
+        return main()
+    else:
+        print('Тест окончен. Нажмите любую клавишу.')
+
+
+if __name__ == '__main__':
+    main()
+    print(sys.stdin.readline())
---
+++ modules/core.py
@@ -0,0 +1,24 @@
+class Question:
+    def __init__(self, question, answer):
+        self.question = question
+        self.answer = answer
+
+
+class KnownTester:
+    def __init__(self, q_list):
+        self.question_number = 0
+        self.score = 0
+        self.question_list = q_list
+
+    def still_has_question(self):
+        return self.question_number < len(self.question_list)
+
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        self.question_number += 1
+        user_answer = int(input(f'Q.{self.question_number}: {current_question.question}> '))
+        self.check_answer(user_answer, current_question.answer)
+
+    def check_answer(self, user_answer, answer):
+        if user_answer == answer:
+            self.score += 1
