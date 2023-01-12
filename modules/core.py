b65d2ebb1aec47d20070b377576f09d664292507
KIRILL <kozlov.khv@gmail.com>
2023-01-12 17:50:57 +1000

    Added check of the entered answer option(2)

--- modules/core.py
+++ modules/core.py
@@ -6,7 +6,7 @@ class Question:
 
 class KnownTester:
     def __init__(self, q_list):
-        self.question_number = 0
+        self.question_number = 1
         self.score = 0
         self.question_list = q_list
 
@@ -14,17 +14,16 @@ class KnownTester:
         return self.question_number < len(self.question_list)
 
     def next_question(self):
-        question_option = [1, 2, 3, 4]
+        question_option = ['1', '2', '3', '4']
         current_question = self.question_list[self.question_number]
-        user_answer = int(input(f'Q.{self.question_number}: {current_question.question}> '))
+        user_answer = input(f'Вопрос №{self.question_number}: {current_question.question}> ')
         if user_answer not in question_option:
             print('Некорректное значение!!!')
             return current_question
         else:
             self.question_number += 1
-        self.check_answer(user_answer, current_question.answer)
+            self.check_answer(user_answer, current_question.answer)
 
     def check_answer(self, user_answer, answer):
-        if user_answer == answer:
+        if user_answer.lower() == answer.lower():
             self.score += 1
-
